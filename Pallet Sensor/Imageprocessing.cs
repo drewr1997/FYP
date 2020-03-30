b09c621bbf5ff919f5c7f01d4a818e0efd68207f
drewr1997 <drewross1997@gmail.com>
2020-03-30 16:08:56 +0100

    format change made to image processing

--- Pallet Sensor/Imageprocessing.cs
+++ Pallet Sensor/Imageprocessing.cs
@@ -11,7 +11,7 @@ using System.Windows.Controls;
 using System.Drawing;
 
 public class Imageprocessing
-{ 
+{
     public static int XRed, YRed, XBlue, YBlue;
     private static Rectangle boundingrectBlue, boundingrectred;
     private static Image<Hsv, Byte> processedred;                   //Casts bitmap to image<Hsv, byte>
@@ -41,17 +41,17 @@ public class Imageprocessing
             CvInvoke.MorphologyEx(Thrred, Thrred, Emgu.CV.CvEnum.MorphOp.Dilate, kernel, new System.Drawing.Point(0, 0), 1, Emgu.CV.CvEnum.BorderType.Default, new MCvScalar(1));
 
             // Used to display red parts of original image
-                    //Extracts only RED parts from orignal image
-                    //Mat Mask;                                                                    //Creates Mat for converting mask to Mat
-                    //Mask = Thrred.Mat;                                                           //Casts mask to Mat
-                    //Image<Hsv, byte> Redisolated = new Image<Hsv, byte>(processedred.Width, processedred.Height);    //Creates Image<Hsv,byte> for final processedred image
-                    //CvInvoke.BitwiseAnd(processedred, processedred, Redisolated, Mask);                     //ANDS mask with orignal image to retain only portions that are RED
+            //Extracts only RED parts from orignal image
+            //Mat Mask;                                                                    //Creates Mat for converting mask to Mat
+            //Mask = Thrred.Mat;                                                           //Casts mask to Mat
+            //Image<Hsv, byte> Redisolated = new Image<Hsv, byte>(processedred.Width, processedred.Height);    //Creates Image<Hsv,byte> for final processedred image
+            //CvInvoke.BitwiseAnd(processedred, processedred, Redisolated, Mask);                     //ANDS mask with orignal image to retain only portions that are RED
 
             //Extracts biggest blob
             //Variables
             double largestareared = 0;
             int largestcontourindexred = 0;
-            Image<Hsv,Byte> Output = new Image<Hsv, Byte>(processedred.Width,processedred.Height);
+            Image<Hsv, Byte> Output = new Image<Hsv, Byte>(processedred.Width, processedred.Height);
             Image<Gray, Byte> ContourdrawnRed = new Image<Gray, Byte>(processedred.Width, processedred.Height);
             VectorOfVectorOfPoint ContoursRed = new VectorOfVectorOfPoint();
             Mat HierarchyRed = new Mat();
@@ -76,10 +76,10 @@ public class Imageprocessing
             YRed = boundingrectred.Y + (boundingrectred.Height / 2);
 
             //Old Method used for overlay
-                    //CvInvoke.DrawContours(processedred, ContoursRed, largestcontourindexred, new MCvScalar(255, 255, 255), 10, Emgu.CV.CvEnum.LineType.Filled, HierarchyRed, 0); //Draws biggest contour on blank image
-                    //processedred.Draw(boundingrectred,new Hsv(255,255,255), 3);
-                    //CvInvoke.Circle(processedred, new System.Drawing.Point(640-XRed, YRed), 4, new MCvScalar(255),2, Emgu.CV.CvEnum.LineType.Filled);
-                    //outputimage.Source = BitmapSourceConvert.ToBitmapSource1(processedred1);
+            //CvInvoke.DrawContours(processedred, ContoursRed, largestcontourindexred, new MCvScalar(255, 255, 255), 10, Emgu.CV.CvEnum.LineType.Filled, HierarchyRed, 0); //Draws biggest contour on blank image
+            //processedred.Draw(boundingrectred,new Hsv(255,255,255), 3);
+            //CvInvoke.Circle(processedred, new System.Drawing.Point(640-XRed, YRed), 4, new MCvScalar(255),2, Emgu.CV.CvEnum.LineType.Filled);
+            //outputimage.Source = BitmapSourceConvert.ToBitmapSource1(processedred1);
 
             //Cleanup
             //Mask.Dispose();
@@ -89,29 +89,29 @@ public class Imageprocessing
 
             //Blue
             Image<Gray, Byte> ThrBlue;                                                     //Creates two Grayscale images that will be used when segmenting
-            ThrBlue = processedred.InRange(new Hsv(85,110,80), new Hsv(135, 230, 220));    //Handles second range for Blue
+            ThrBlue = processedred.InRange(new Hsv(85, 110, 80), new Hsv(135, 230, 220));    //Handles second range for Blue
 
             //Handles noise and cleans image
             CvInvoke.MorphologyEx(ThrBlue, ThrBlue, Emgu.CV.CvEnum.MorphOp.Open, kernel, new System.Drawing.Point(0, 0), 1, Emgu.CV.CvEnum.BorderType.Default, new MCvScalar(1));
             CvInvoke.MorphologyEx(ThrBlue, ThrBlue, Emgu.CV.CvEnum.MorphOp.Dilate, kernel, new System.Drawing.Point(0, 0), 1, Emgu.CV.CvEnum.BorderType.Default, new MCvScalar(1));
 
             //Used to display blue parts of original image
-                    //Extracts only RED parts from orignal image
-                    //Mat Mask1;                                                                  //Creates Mat for converting mask to Mat
-                    //Mask1 = ThrBlue.Mat;                                                           //Casts mask to Mat
-                    //Image<Hsv, byte> Bluleisolated = new Image<Hsv, byte>(processedred.Width, processedred.Height);    //Creates Image<Hsv,byte> for final processedred image
-                    //CvInvoke.BitwiseAnd(processedred, processedred, Redisolated, Mask);                     //ANDS mask with orignal image to retain only portions that are RED
+            //Extracts only RED parts from orignal image
+            //Mat Mask1;                                                                  //Creates Mat for converting mask to Mat
+            //Mask1 = ThrBlue.Mat;                                                           //Casts mask to Mat
+            //Image<Hsv, byte> Bluleisolated = new Image<Hsv, byte>(processedred.Width, processedred.Height);    //Creates Image<Hsv,byte> for final processedred image
+            //CvInvoke.BitwiseAnd(processedred, processedred, Redisolated, Mask);                     //ANDS mask with orignal image to retain only portions that are RED
 
             //Extracts biggest blob
             //Variables
             double LargestAreaBlue = 0;
             int LargestContourIndexBlue = 0;
-            MCvPoint2D64f CenterBlue = new MCvPoint2D64f(0,0);
+            MCvPoint2D64f CenterBlue = new MCvPoint2D64f(0, 0);
             Image<Gray, Byte> ContourDrawnBlue = new Image<Gray, Byte>(processedred.Width, processedred.Height);
             VectorOfVectorOfPoint ContoursBlue = new VectorOfVectorOfPoint();
             Moments MomentsBlue = new Moments();
             Mat HierarchyBlue = new Mat();
-             
+
             //Processing
             CvInvoke.FindContours(ThrBlue, ContoursBlue, HierarchyBlue, Emgu.CV.CvEnum.RetrType.Ccomp, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);    //Finds contours in image
 
@@ -152,75 +152,76 @@ public class Imageprocessing
 
             return;
         }
-        else {return;}
+        else { return; }
     }
 
     //Create output screen
     public static void OutputScreen(System.Windows.Controls.Image outputimage, double[] frame)
     {
-            Image<Hsv, byte> blank = new Image<Hsv, byte>(processedred.Width, processedred.Height, new Hsv(100, 100, 100)); //Creates new blank image
-            double angle = Math.Atan(frame[8] / frame[0]);                                         //Compute the horizontal angle betwwen pallet and Kinect
-            string distance = "DISTANCE =";
-            int xscaled, x2, x3, zscaled, y2, y3, yscaled, w=150, i=0;
+        Image<Hsv, byte> blank = new Image<Hsv, byte>(processedred.Width, processedred.Height, new Hsv(100, 100, 100)); //Creates new blank image
+        double angle = Math.Atan(frame[8] / frame[0]);                                         //Compute the horizontal angle betwwen pallet and Kinect
+        string distance = "DISTANCE =";
+        int xscaled, x2, x3, zscaled, y2, y3, yscaled, w = 150, i = 0;
 
-            //Scaling pallet centre to fit the screenoi nij9h
-            xscaled = Convert.ToInt32(320 - (frame[3] / 6000 * 640));
-            yscaled = Convert.ToInt32(((frame[7]+400) / 4096 * 150));
-            zscaled = Convert.ToInt32(480 - (frame[11] / 6000 * 480));
+        //Scaling pallet centre to fit the screenoi nij9h
+        xscaled = Convert.ToInt32(320 - (frame[3] / 6000 * 640));
+        yscaled = Convert.ToInt32(((frame[7] + 400) / 4096 * 150));
+        zscaled = Convert.ToInt32(480 - (frame[11] / 6000 * 480));
 
-            //Drawing the arrow
-            CvInvoke.Line(blank, new System.Drawing.Point(320, 480), new System.Drawing.Point(320, 430), new MCvScalar(0, 0, 255), 5, Emgu.CV.CvEnum.LineType.Filled);
-            CvInvoke.Line(blank, new System.Drawing.Point(320, 430), new System.Drawing.Point(300, 450), new MCvScalar(0, 0, 255), 5, Emgu.CV.CvEnum.LineType.Filled);
-            CvInvoke.Line(blank, new System.Drawing.Point(320, 430), new System.Drawing.Point(340, 450), new MCvScalar(0, 0, 255), 5, Emgu.CV.CvEnum.LineType.Filled);
+        //Drawing the arrow
+        CvInvoke.Line(blank, new System.Drawing.Point(320, 480), new System.Drawing.Point(320, 430), new MCvScalar(0, 0, 255), 5, Emgu.CV.CvEnum.LineType.Filled);
+        CvInvoke.Line(blank, new System.Drawing.Point(320, 430), new System.Drawing.Point(300, 450), new MCvScalar(0, 0, 255), 5, Emgu.CV.CvEnum.LineType.Filled);
+        CvInvoke.Line(blank, new System.Drawing.Point(320, 430), new System.Drawing.Point(340, 450), new MCvScalar(0, 0, 255), 5, Emgu.CV.CvEnum.LineType.Filled);
 
-            //Drawing dashed centre line
-            i = 0;
-            while (i < 480){
-                CvInvoke.Line(blank, new System.Drawing.Point(320, i), new System.Drawing.Point(320, i+20), new MCvScalar(0, 0, 255), 5, Emgu.CV.CvEnum.LineType.Filled);
-                i += 40;
-            }
+        //Drawing dashed centre line
+        i = 0;
+        while (i < 480)
+        {
+            CvInvoke.Line(blank, new System.Drawing.Point(320, i), new System.Drawing.Point(320, i + 20), new MCvScalar(0, 0, 255), 5, Emgu.CV.CvEnum.LineType.Filled);
+            i += 40;
+        }
 
-            i = 0;
-            while (i < 200)
-            {
-            CvInvoke.Line(blank, new System.Drawing.Point(245, 480-i), new System.Drawing.Point(245, 480-i + 10), new MCvScalar(0, 0, 255), 3, Emgu.CV.CvEnum.LineType.Filled);
-            CvInvoke.Line(blank, new System.Drawing.Point(395, 480-i), new System.Drawing.Point(395, 480-i + 10), new MCvScalar(0, 0, 255), 3, Emgu.CV.CvEnum.LineType.Filled);
+        i = 0;
+        while (i < 200)
+        {
+            CvInvoke.Line(blank, new System.Drawing.Point(245, 480 - i), new System.Drawing.Point(245, 480 - i + 10), new MCvScalar(0, 0, 255), 3, Emgu.CV.CvEnum.LineType.Filled);
+            CvInvoke.Line(blank, new System.Drawing.Point(395, 480 - i), new System.Drawing.Point(395, 480 - i + 10), new MCvScalar(0, 0, 255), 3, Emgu.CV.CvEnum.LineType.Filled);
             i += 20;
-            }
+        }
 
         //Drawing vertical compass
-        CvInvoke.Rectangle(blank, new Rectangle(90,15, 30, 300), new MCvScalar(0, 0, 255), 5, Emgu.CV.CvEnum.LineType.Filled);
+        CvInvoke.Rectangle(blank, new Rectangle(90, 15, 30, 300), new MCvScalar(0, 0, 255), 5, Emgu.CV.CvEnum.LineType.Filled);
         CvInvoke.Line(blank, new System.Drawing.Point(15, 240), new System.Drawing.Point(45, 240), new MCvScalar(0, 0, 255), 3, Emgu.CV.CvEnum.LineType.Filled);
         CvInvoke.PutText(blank, "Height", new System.Drawing.Point(15, 60), Emgu.CV.CvEnum.FontFace.HersheyPlain, 1.5, new MCvScalar(0, 255, 255), 2, Emgu.CV.CvEnum.LineType.Filled);
 
         if (frame[3] != 0 && frame[11] != 0)
-            {
-                //Drawing circle on centre of pallet
-                CvInvoke.Circle(blank, new System.Drawing.Point(xscaled, zscaled), 5, new MCvScalar(180, 255, 255), 2, Emgu.CV.CvEnum.LineType.Filled);
+        {
+            //Drawing circle on centre of pallet
+            CvInvoke.Circle(blank, new System.Drawing.Point(xscaled, zscaled), 5, new MCvScalar(180, 255, 255), 2, Emgu.CV.CvEnum.LineType.Filled);
 
-                //Computing end line coordinates
-                x2 = Convert.ToInt32(xscaled - (w / 2) * Math.Cos(angle));
-                y2 = Convert.ToInt32(zscaled - (w / 2) * Math.Sin(angle));
-                x3 = Convert.ToInt32(xscaled + (w / 2) * Math.Cos(angle));
-                y3 = Convert.ToInt32(zscaled + (w / 2) * Math.Sin(angle));
+            //Computing end line coordinates
+            x2 = Convert.ToInt32(xscaled - (w / 2) * Math.Cos(angle));
+            y2 = Convert.ToInt32(zscaled - (w / 2) * Math.Sin(angle));
+            x3 = Convert.ToInt32(xscaled + (w / 2) * Math.Cos(angle));
+            y3 = Convert.ToInt32(zscaled + (w / 2) * Math.Sin(angle));
 
-                //Drawing line for the pallet
-                CvInvoke.Line(blank, new System.Drawing.Point(x2, y2), new System.Drawing.Point(x3, y3), new MCvScalar(180, 255, 255), 5, Emgu.CV.CvEnum.LineType.Filled);
+            //Drawing line for the pallet
+            CvInvoke.Line(blank, new System.Drawing.Point(x2, y2), new System.Drawing.Point(x3, y3), new MCvScalar(180, 255, 255), 5, Emgu.CV.CvEnum.LineType.Filled);
 
-                //Drawing of line on vertical compass
-                CvInvoke.Line(blank, new System.Drawing.Point(15, 240 + yscaled), new System.Drawing.Point(45, 240 + yscaled), new MCvScalar(180, 255, 255), 4, Emgu.CV.CvEnum.LineType.Filled);
-                distance = ($"Distance = {(frame[11]-1200)/1000}m");
-                CvInvoke.PutText(blank, distance, new System.Drawing.Point(350, 30), Emgu.CV.CvEnum.FontFace.HersheyPlain, 1.5, new MCvScalar(0, 255, 255), 2, Emgu.CV.CvEnum.LineType.Filled);
-            }
-            else
-            {
-                //Drawing of line on vertical compass
-                CvInvoke.Line(blank, new System.Drawing.Point(15, 240 - yscaled), new System.Drawing.Point(45, 240 - yscaled), new MCvScalar(180, 255, 255), 4, Emgu.CV.CvEnum.LineType.Filled);
-                distance = ($"Distance = {(frame[11] -1200) / 1000}m");
-                CvInvoke.PutText(blank, "No Pallet Found", new System.Drawing.Point(350, 30), Emgu.CV.CvEnum.FontFace.HersheyPlain, 1.5, new MCvScalar(0, 255, 255), 2, Emgu.CV.CvEnum.LineType.Filled);
-            }
+            //Drawing of line on vertical compass
+            CvInvoke.Line(blank, new System.Drawing.Point(15, 240 + yscaled), new System.Drawing.Point(45, 240 + yscaled), new MCvScalar(180, 255, 255), 4, Emgu.CV.CvEnum.LineType.Filled);
+            distance = ($"Distance = {(frame[11] - 1200) / 1000}m");
+            CvInvoke.PutText(blank, distance, new System.Drawing.Point(350, 30), Emgu.CV.CvEnum.FontFace.HersheyPlain, 1.5, new MCvScalar(0, 255, 255), 2, Emgu.CV.CvEnum.LineType.Filled);
+        }
+        else
+        {
+            //Drawing of line on vertical compass
+            CvInvoke.Line(blank, new System.Drawing.Point(15, 240 - yscaled), new System.Drawing.Point(45, 240 - yscaled), new MCvScalar(180, 255, 255), 4, Emgu.CV.CvEnum.LineType.Filled);
+            distance = ($"Distance = {(frame[11] - 1200) / 1000}m");
+            CvInvoke.PutText(blank, "No Pallet Found", new System.Drawing.Point(350, 30), Emgu.CV.CvEnum.FontFace.HersheyPlain, 1.5, new MCvScalar(0, 255, 255), 2, Emgu.CV.CvEnum.LineType.Filled);
+        }
 
-            //Displaying the output image to user interface
-            outputimage.Source = BitmapSourceConvert.ToBitmapSource1(blank);
+        //Displaying the output image to user interface
+        outputimage.Source = BitmapSourceConvert.ToBitmapSource1(blank);
     }
 }
